44ce0c0dded536265e922d8114988ee15619ab3b
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2023-11-19 11:15:17 -0300

    Criando uma função para tratar o inteiro do teclado

--- main_dart.dart
+++ main_dart.dart
@@ -10,3 +10,24 @@ int? somatorio(int numero) {
   }
   return somatorio;
 }
+
+int trataInteiro(int numero) {
+  bool condition;
+  do {
+    print("Insira um número inteiro positivo: ");
+    String? entrada = stdin.readLineSync();
+    try {
+      int numero = int.parse(entrada!);
+      print('Você digitou: $numero');
+    } catch (e) {
+      print('Por favor, digite um número inteiro válido.');
+    }
+    numero.isNegative ? condition = true : condition = false;
+
+    if (condition)
+      print("O Número nao é um inteiro Positivo, favor inserir  outro valor: ");
+  } while (condition);
+  return numero;
+}
+
+void main(List<String> args) {}
