fed092ce5ae5ff25a1b16bb41ad1661a2102e717
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2023-11-19 10:59:43 -0300

    Criando o corpo da função somatorio

--- main_dart.dart
+++ main_dart.dart
@@ -1,3 +1,12 @@
 import 'dart:io';
 
-int? somatorio(int numero) {}
+int? somatorio(int numero) {
+  var somatorio = 0;
+
+  for (var i = 0; i < numero; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      somatorio += i;
+    }
+  }
+  return somatorio;
+}
